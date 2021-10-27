963d1348840cba48cba6fee0795db4e2436932f2
JEH <han880330@yahoo.co.jp>
2021-10-27 19:18:53 +0900

    javascript形式のデータをVue形式に変更、新しいファイルコンポーネントを作成、中のデータを分析,バグを修正

---
+++ src/components/School.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>name: {{schoolName}}</h2>
+        <h2>address: {{address}}</h2>
+        <button @click="showName">Click to show school name</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'School',
+        data() {
+            return {
+                schoolName:'Google',
+                address:'NewYork'
+            }
+        },
+        methods: {
+            showName(){
+                alert(this.schoolName)
+            }
+        },
+    }
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Student name: {{name}}</h2>
+        <h2>Student age: {{age}}</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'Student',
+        data() {
+            return {
+                name:'baidu',
+                age:100
+            }
+        }
+    }
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
