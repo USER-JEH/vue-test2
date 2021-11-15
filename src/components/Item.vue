ace2a26a084fee9714083e0342ba7045147b3a89
JEH <han880330@yahoo.co.jp>
2021-11-15 15:50:43 +0900

    nextTick

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,13 +1,18 @@
 <template>
    <li>
         <label>
-            <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
+            <input 
+                type="checkbox" 
+                :checked="todo.done" 
+                @change="handleCheck(todo.id)"
+            />
             <span v-show="!todo.isEdit">{{ todo.title }}</span>
             <input 
                 type="text" 
                 v-show="todo.isEdit" 
                 :value="todo.title" 
                 @blur="handleBlur(todo,$event)"
+                ref="inputTitle"
             >
         </label>
         
@@ -44,6 +49,10 @@ export default {
                 console.log('todo have no isEdit')
                 this.$set(todo,'isEdit',true)
             }
+            this.$nextTick(function(){
+                this.$refs.inputTitle.focus()
+            })
+            //
         },
         handleBlur(todo,e){
             todo.isEdit = false
