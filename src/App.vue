2c48ce6b39cc7fec5f5d74151548b114f4290dd1
JEH <han880330@yahoo.co.jp>
2021-11-17 18:17:03 +0900

    animation effect,enter,leave-to,enter-to,leave

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
  <div>
   <Test/>
+  <Test2/>
  </div>
 </template>
 
 <script>
 import Test from './component/Test'
+import Test2 from './component/Test2'
 
 export default {
   name: 'App',
-  components:{Test},
+  components:{Test,Test2},
 }
 </script>
 
---
+++ src/component/Test2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <button @click="isShow = !isShow">show/hide</button>
+      <transition name="hello" appear>
+        <h1 v-show="isShow">Hello</h1>
+      </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Test',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+
+<style>
+    h1{
+        background-color: orange;
+    }
+
+    /* 进入和离开的起点 */
+    .hello-enter,.hello-leave-to{
+      transform: translateX(-100%);
+    }
+    .hello-enter-active,.hello-leave-active{
+        transition: 0.5s linear;
+    }
+    /* 进入和离开的终点*/
+    .hello-enter-to,.hello-leave{
+      transform: translateX(0);
+    }
+
+
+</style>
