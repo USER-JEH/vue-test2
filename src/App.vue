b4bf70984f6ae42b82d1f8ea0edbf34f72876b0b
JEH <han880330@yahoo.co.jp>
2021-11-19 17:34:51 +0900

    要素の入れ替え、animate

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
  <div>
   <Test/>
   <Test2/>
+  <Test3/>
  </div>
 </template>
 
 <script>
 import Test from './component/Test'
 import Test2 from './component/Test2'
+import Test3 from './component/Test3'
 
 export default {
   name: 'App',
-  components:{Test,Test2},
+  components:{Test,Test2,Test3},
 }
 </script>
 
---
+++ src/component/Test3.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <button @click="isShow = !isShow">show/hide</button>
+      <transition-group 
+        appear
+        name="animate__animated animate__bounce" 
+        enter-active-class="animate__swing"
+        leave-active-class="animate__backOutUp"
+      >
+        <h1 v-show="!isShow" key="1">Hello</h1>
+        <h1 v-show="isShow" key="2">Goodbye</h1>
+      </transition-group>
+  </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+    name:'Test',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+
+<style>
+    h1{
+        background-color: orange;
+    }
+
+
+</style>
